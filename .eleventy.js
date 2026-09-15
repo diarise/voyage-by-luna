2097ac7fd86057bc322b6d96cf3bc61f6aecb30e
Daouda Dia <daouda@Daoudas-MacBook-Pro.local>
2026-09-15 11:39:13 -0700

    Copy website assets during Eleventy build

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("CNAME");
+
   return {
     dir: {
       input: "src",
